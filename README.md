10c6669f14b6ddd0aa43081cb8fc28bb9c477c45
Yev <56968596+Yevean23@users.noreply.github.com>
2023-10-25 10:33:15 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# Num-2-XL-Col
+
+Num-2-XL-Col is a lightweight utility package designed to simplify the conversion of numeric values to Excel column names, adhering to Excel's unique column naming convention. In Excel, the letter 'A' is not just the first column but also represents 1 when considering columns beyond the ones digit. This package handles this intricacy effortlessly, saving you valuable time and effort in your software engineering tasks.
+
+
+## Features
+
+Effortless Conversion: Convert numeric values to Excel column names seamlessly, handling Excel's complex column naming rules behind the scenes.
+
+Flexible Indexing: Whether you prefer 0-based or 1-based indexing, Num-2-XL-Col supports both, accommodating various use cases in your projects.
+
+
+## Usage
+
+```
+const { numToExcelCol, excelColtoNum } = require('num-2-xl-col');
+
+console.log(numToExcelCol(0)); // Output: 'A'
+console.log(numToExcelCol(1, oneBased=true)); // Output: 'A'
+console.log(excelColtoNum('AA')); // Output: 26
+console.log(excelColtoNum('AA', oneBased=true)); // Output: 27
+```
+
+
+## Feature Requests and Contributions
+If you have ideas for enhancing this package or encounter any issues, please don't hesitate to reach out via email at vovk.yev@gmail.com. Your feedback and suggestions are highly appreciated!
+
+
+## License
+This project is licensed under the MIT License.
---
+++ index.js
@@ -0,0 +1,63 @@
+function numToExcelCol(num, oneBased = false) {
+    if (typeof (num) != "number") {
+        console.log('numToExcelCol expected a number');
+    }
+
+    if (num < 0) {
+        console.log('number should be greater than 0');
+    }
+    let ret = '';
+
+    if (oneBased) {
+        while (num > 0) {
+            num--; // Adjust num to handle 1-based indexing.
+            const remainder = num % 26;
+            const char = String.fromCharCode(remainder + 65); // Convert remainder to character.
+            ret = char + ret; // Append the character to the result.
+            num = Math.floor(num / 26); // Update num for the next iteration.
+        }
+    } else {
+        if (num === 0) {
+            return 'A';
+        }
+        didFirst = 0;
+        while (num > 0) {
+            let i = num % 26;
+            let n = String.fromCharCode(i + 65 - didFirst);
+            ret = n + ret;
+            num = Math.floor(num / 26);
+            didFirst = 1;
+        }
+    }
+
+    return ret;
+}
+
+function excelColtoNum(colname, oneBased = false) {
+    let total = 0;
+    for (let i = 0; i < colname.length; i++) {
+        total *= 26; // Multiply the total by 26 for each position to simulate base 26.
+        total += colname.charCodeAt(i) - 64; // Convert the character to its corresponding number and add to the total.
+    }
+    return oneBased ? total : total - 1;
+}
+
+/*
+TESTS FOR VERIFICATION OF FUNCTIONALITY
+I WASN'T ABOUT TO INCLUDE JEST AS A DEV DEPENDENCY JUST FOR TESTING PURPOSES.
+
+for (let ir = 0; ir <= 100; ir++) {
+    let ci = numToExcelCol(ir);
+    let b = excelColtoNum(ci);
+    console.log(ir, ci, b)
+}
+
+for (let ir = 0; ir <= 100; ir++) {
+    let ci = numToExcelCol(ir, true);
+    let b = excelColtoNum(ci, true);
+    console.log(ir, ci, b)
+}
+
+*/
+
+module.exports = [numToExcelCol, excelColtoNum]
